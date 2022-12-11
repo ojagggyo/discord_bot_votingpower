3515d09c51313fc3b1a80eb642cf81eb5b31f095
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2022-12-11 10:18:56 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,7 @@ const Discord = require('discord.js');
 require('dotenv').config();
 
 // Discord Clientのインスタンス作成
-//const client = new Discord.Client();
-//const client = new Discord.Client({ intents: ["everyone"] });
-const client = new Discord.Client({ intents: ["DIRECT_MESSAGES", "GUILD_MESSAGES"] });
+const client = new Discord.Client();
 
 // トークンの用意
 const token = process.env.TOKEN;
@@ -18,22 +16,17 @@ client.on('ready', () => {
 client.on('message', message => {
     if(message.author.bot) return; //BOTのメッセージには反応しない
 
-//    if(message.content === "/hello"){ //送られたメッセージが /helloだったら
-//        message.channel.send("HELLO!")
-//        //メッセージが送られたチャンネルに HELLO!と送信する
-//    }
-    
-      //message.channel.send(message.content);
-    
-    const username = message.content;
-    
+    const content = message.content;
+    let username = content.replace(/^<@[0-9]+>[ ]?/, ''); //<@1051077091183034398>を除く
+    username = username.toLowerCase(); //小文字に変換する
+
         const dsteem = require('dsteem');
         const client = new dsteem.Client('https://api.steememory.com');
         console.log(`username=${username}`);
         client.database.call('get_accounts', [[username]])
         .then(result => {
             let vp = result[0].voting_power + (10000 * ((new Date() - new Date(result[0].last_vote_time + "Z")) / 1000) / 432000);
-            vp = vp / 100;            
+            vp = vp / 100;
             message.channel.send(`こんにちは、${username}さん`);
             message.channel.send(`Voting Powerは、${vp.toFixed(1)}です。`);
             message.channel.send(`https://steemit.com/@${username}/posts`);
